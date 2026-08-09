dd60a92666cd053f9190d1552eccae694bc99f94
WalterCrace <mq20003@ues.edu.sv>
2026-08-09 14:46:52 -0600

    Agregado CatalogoController y configuradas las rutas web

--- app/Http/Controllers/CatalogoController.php
+++ app/Http/Controllers/CatalogoController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Lugar;
 
 class CatalogoController extends Controller
 {
-    //
+    public function index()
+    {
+        $lugares = Lugar::all();
+        return view('catalogo.index', compact('lugares'));
+    }
+
+    public function show($id)
+    {
+        $lugar = Lugar::find($id);
+
+        if (!$lugar){
+            abort(404, 'Lugar turístico no encontrado');
+        }
+
+        return view('catalogo.detalle', compact('lugar'));
+    }
 }
